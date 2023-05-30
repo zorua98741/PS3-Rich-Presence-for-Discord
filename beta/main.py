9434ba33d6f23a018f2875ee2290c6e8d67e9003
zorua98741 <83170597+zorua98741@users.noreply.github.com>
2023-05-30 22:40:28 +1000

    Create main.py
    backup of in-progress v1.8.1.
    "PrepWork" class is complete - pypresence setup

---
+++ beta/main.py
@@ -0,0 +1,131 @@
+from socket import socket, AF_INET, SOCK_DGRAM  # used to get host IP address
+import re  # used for regular expressions
+import networkscan  # used for automatic obtaining IP address, requires pip install
+import os  # used to test if config exists
+import requests  # used to test if given IP belongs to PS3, requires pip install
+from requests.exceptions import ConnectionError  # used to handle thrown errors on connecting to webpage
+from bs4 import BeautifulSoup   # used for webpage scraping, requires pip install
+# from pypresence import Presence, InvalidPipe, InvalidID     # pypresence requires pip install
+
+
+class PrepWork:     # Python2 class should be "class PrepWork(object):" ?
+    def __init__(self):
+        self.ip = None
+        self.clientID = '780389261870235650'
+        self.waitTime = '35'  # seconds
+        self.showTemps = 'True'
+        self.indivRetroCovers = 'False'
+        self.resetTimer = 'False'
+
+    def read_config(self):
+        if os.path.isfile('PS3RPDconfig.txt'):  # test if file exists
+            file = open('PS3RPDconfig.txt', 'r')
+            lines = file.readlines()
+            try:    # overwrite default values with values stored in config file
+                # ! regex could be improved !
+                self.ip = re.search('([0-9]{1}.*)', lines[0]).group(1)  # capture start at 1 or more number to end of string
+                self.clientID = re.search(': (.*)', lines[1]).group(1)  # capture start after ": " to end of string
+                self.waitTime = re.search(': (.*)', lines[2]).group(1)
+                if int(self.waitTime) < 15:     # waitTime is limited by Discord, do not allow it to be less than 15
+                    self.waitTime = "15"
+                self.showTemps = re.search(': (.*)', lines[3]).group(1)
+                self.indivRetroCovers = re.search(': (.*)', lines[4]).group(1)
+                self.resetTimer = re.search(': (.*)', lines[5]).group(1)
+            except Exception as e:
+                exit(f'error with config file "{e}".')
+            if self.test_for_webman(self.ip):    # IP in config still belongs to PS3
+                pass    # ! connect and begin gathering details from webman here !
+            else:   # IP in config does not belong to PS3
+                print('IP in config file is not detected as belonging to a PS3, please check if it has changed.')
+                self.promptUser()
+        else:   # file does not exist
+            print('no config found!')
+            self.promptUser()
+
+    def promptUser(self):
+        choice = 'placeholder'
+        accepted = ['a', 'm']
+        print("Get PS3's IP address automatically, or manually?")
+        while choice[0].lower() not in accepted:  # test if first character of choice is in array
+            choice = input('Please enter either "A", or "M": ')
+        if choice[0].lower() == 'a':
+            self.grab_host_network()
+        elif choice[0].lower() == 'm':
+            self.get_IP_from_user()
+        else:
+            exit('Unexpected input')
+
+    def grab_host_network(self):  # hard to account for what users firewall and network will look like. Function may not work for all users.
+        hostNetwork = None
+        try:
+            tempSock = socket(AF_INET, SOCK_DGRAM)
+            tempSock.connect(('8.8.8.8', 80))
+            hostNetwork = tempSock.getsockname()[0]
+            tempSock.close()
+        except Exception as e:
+            print(f'Error while getting host network. "{e}"')
+
+        if hostNetwork is not None:
+            hostNetwork = re.search('^(.*)\.', hostNetwork).group(0)  # remove machine's octet
+            print(f'expected network is "{hostNetwork}"')
+            self.scan_network(hostNetwork)
+
+    def scan_network(self, my_network):  # takes IPv4 address in form 'x.x.x.'
+        # adapted from nychron's code
+        my_network += '0/24'  # append 4th octet and short-form subnet mask
+        my_scan = networkscan.Networkscan(my_network)
+
+        # Run the scan of hosts using pings
+        my_scan.run()
+
+        # Display the IP address of all the hosts found
+        print('Completed network scan.')
+        print(my_scan.list_of_hosts_found)
+
+        for i in range(len(my_scan.list_of_hosts_found)):
+            if self.test_for_webman(my_scan.list_of_hosts_found[i]):
+                self.save_config(my_scan.list_of_hosts_found[i])
+                break   # do not test further IPs if one is found to belong to webman
+    # ! NEED TO HANDLE IF PS3 IS NOT FOUND !
+
+    def get_IP_from_user(self):
+        while True:
+            ip = input("Enter PS3's IP address: ")
+            if self.test_for_webman(ip):
+                self.save_config(ip)
+                break
+
+    def test_for_webman(self, ip):
+        response = None
+        url = f'http://{ip}'
+        headers = {'Content-Type': 'text/html'}  # Alternatively {"User-Agent": "Mozilla/5.0"}
+        try:    # test if ANY webpage is running on given IP
+            response = requests.get(url, headers=headers)
+        except ConnectionError as e:
+            print(f'No webpage found on "{ip}"')
+            return False
+        if response is not None:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            pageTitle = str(soup.find('title'))     # default type is bs4.element.tag, needs to be string
+            if 'wMAN' in pageTitle or 'webMAN' in pageTitle:    # test for known value in <title>
+                print(f'given IP "{ip}" belongs to webman.')
+                return True
+            else:   # a webpage was found, but does not satisfy check to belong to webman
+                print(f'WebmanMOD not found on "{ip}", reports "{pageTitle}". If you believe this is an error, please contact the developer. '
+                      f'Please ensure the PS3 is turned on, has webmanMOD installed and running, '
+                      f'and is connected to the same network as the PC.')
+                return False
+
+    def save_config(self, validatedIP):     # updates/creates the external config file
+        print("Updating/Creating config with saved values")
+        file = open('PS3RPDconfig.txt', 'w+')   # w+ creates file if it doesn't exist, overwriting previous content
+        file.write(f'IP: {validatedIP}\n')
+        file.write(f'clientID: {self.clientID}\n')  # use config file values if it exists, otherwise default values
+        file.write(f'wait time: {self.waitTime}\n')
+        file.write(f'show temperature: {self.showTemps}\n')
+        file.write(f'use individual art for retro games: {self.indivRetroCovers}\n')
+        file.write(f'reset time elapsed on new game: {self.resetTimer}\n')
+
+
+prepWork = PrepWork()
+prepWork.read_config()
